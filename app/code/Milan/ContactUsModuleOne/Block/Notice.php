aa9262a74676042981fb1ea76b0d2833d6812338
Milan Kumar Pani <milankumarpani@gmail.com>
2022-11-28 19:59:27 +0530

    feature\event-and-plugin

---
+++ app/code/Milan/ContactUsModuleOne/Block/Notice.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Milan\ContactUsModuleOne\Block;
+
+class Notice extends \Magento\Framework\View\Element\Template
+{
+
+    public function getMessage()
+    {
+        return "Message On Top Of Contact Notice Module (One)";
+    }
+}
